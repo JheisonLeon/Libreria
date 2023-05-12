07273a58c1b7dfd71ba2aa358ae446acb37c4b46
Jheison Leon <jheisonjr15@gmail.com>
2023-05-12 17:11:49 -0400

    add local bootstrap-config

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'')
 MEDIA_URL = '/imagenes/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)#['/static/']
